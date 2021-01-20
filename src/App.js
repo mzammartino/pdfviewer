1a44e5ed7fa09e35d3c1271e55c5899fc0d1ab2c
Mirko Zammartino <zammartino.m@gmail.com>
2021-01-20 18:10:10 +0100

    Nothing particular

--- src/App.js
+++ src/App.js
@@ -4,20 +4,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="toolbar">
+
+      </div>
+      <div className="main">
+      <div className="preview"></div>
+      <div className="view"></div>
+      </div>
     </div>
   );
 }
